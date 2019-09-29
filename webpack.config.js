72e4ab4dd5f3e831a1cab6265df4084d0a12f080
Jihaad Pienaar <jihaad.pienaar@spandigital.com>
2019-09-29 16:53:39 +0200

    Added webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var HtmlWebpackPlugin =  require('html-webpack-plugin');
+
+module.exports = {
+  entry : './src/index.js',
+  output : {
+    path : path.resolve(__dirname , 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module : {
+    rules : [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: 'babel-loader', // THIS IS FOR ES6 SUPPORT
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader']
+      },
+    ]
+  },
+  mode:'development',
+  plugins : [
+    new HtmlWebpackPlugin ({
+      template : 'src/index.html'
+    })
+  ]
+
+}
